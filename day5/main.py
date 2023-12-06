0fff3d686d704ca2f1569e4ac78bd2213b077b3c
Anson <tmmanson77@gmail.com>
2023-12-06 18:34:09 +0000

    update day6 solution

--- day5/main.py
+++ day5/main.py
@@ -1,69 +1,106 @@
-from collections import defaultdict
-
-
-def func1(fname ='test.py'):
-    ans = 0
+def func1(fname='test.txt'):
+    fourdarry = []
     with open(fname) as f:
-        for idx,linef in enumerate(f):
-            line = linef.strip()
-            [header_part1, part2]  = line.split('| ')
-            part1 = header_part1.split(':')[1]
-
-            left = set()
-            right = set()
-            for n in part1.split():
-                if n.isdigit():
-                    left.add(int(n))
-            for n in part2.split():
-                if n.isdigit():
-                    right.add(int(n))
-            # part1 e.g. ' 41 48 83 86 17 ' get the numbers 
-            ans +=score(len(left.intersection(right)))
-    return ans
-
-def score(n):
-    if n == 0:
-        return 0
-    return 2**(n-1)
-print(func1('test.txt'))
-print(func1('input.txt'))
-
-def func2(fname ='test.py'):
+        iterf = iter(f)
+        seeds = next(iterf) 
+        seeds = [int(n) for n in seeds.strip().split(' ')[1:]]
+        print(seeds)
+        nmap = -1
+        for line in f:
+            if line == '\n':
+                fourdarry.append([])
+                nmap += 1
+            else:
+                fourdarry[nmap].append(line.strip().split())
+                    
+                
+    def seed_to_location(seed):
+        print('seed is :',seed)
+        for si,step in enumerate(fourdarry):
+            for ci, choice in enumerate(fourdarry[si][1:]):
+                # print(f'choice:{choice}')
+                [d, s, r] = [int(n) for n in choice]
+                if s<= seed< s+r:
+                    seed+= (d-s)
+                    break
+        return seed
+    
     ans = 0
-    lines = []
+    min_loc = float('inf')
+    for seed in seeds:
+        tmp = seed_to_location(seed)
+        if tmp < min_loc:
+            min_loc=tmp
+            ans = seed
+        
+        
+    return min_loc
+
+        
+
+
+# print(func1('input.txt'))
+        
+def func2(fname='test.txt'):
+    fourdarry = []
     with open(fname) as f:
-        for idx,linef in enumerate(f):
-            lines.append(linef.strip())
-    card_to_point = [0]*(len(lines))
-    # length of card_number is the number of cards
-    card_number = [0]*(len(lines))
+        iterf = iter(f)
+        seeds = next(iterf) 
+        seeds = [int(n) for n in seeds.strip().split(' ')[1:]]
+        seeds_pair = [seeds[npair:npair+2] for npair in range(0,len(seeds),2)]
+        seeds_pair =  [(x, x+y) for (x,y) in seeds_pair] # convert to range
+        seeds = seeds_pair
+        print(seeds)
+        nmap = -1
+        for line in f:
+            if line == '\n':
+                fourdarry.append([])
+                nmap += 1
+            else:
+                fourdarry[nmap].append(line.strip().split())
+                    
+                
 
-    for idx, line in  enumerate(lines):
-        [header_part1, part2]  = line.split('| ')
-        part1 = header_part1.split(':')[1]
-        left = set()
-        right = set()
-        for n in part1.split():
-            if n.isdigit():
-                left.add(int(n))
-        for n in part2.split():
-            if n.isdigit():
-                right.add(int(n))
-        # part1 e.g. ' 41 48 83 86 17 ' get the numbers 
-        card_to_point[idx] = len(left.intersection(right))
-        card_number[idx] +=1
+    
+    def seed_range_to_location_min(seed_ranges):
+        # seed_range is a [] contains range e.g. [(1,3),(5,8)]
+        curr_remain= seed_ranges.copy()
 
-    print(card_to_point)
-    print(card_number)
-    for i in range(len(lines)): # iterate all no.cards
-        if card_number[i] > 0: 
-            # ans += card_number[i]
-            print(i, card_to_point[i], card_number[i])
-            # if i + card_to_point[i]+1 < len(lines):
-            for j in range(i+1,i+1+card_to_point[i]):
-                card_number[j] += card_number[i] 
+        for si,step in enumerate(fourdarry):
+            # hit = 0
+            next_step = set()
+            for ci, choice in enumerate(step[1:]):
+                [d, s, r] = [int(n) for n in choice]
+                print(f'choice {ci}', choice)
+                offset = d-s
+                seed_ranges = curr_remain.copy()
+                for  seed_minmax in seed_ranges:
+                    (seed_min, seed_max)  = seed_minmax
+                    curr_remain.remove(seed_minmax)
+                    # intersection:
+                    if max(seed_min,s) < min(seed_max,s+r) and seed_max > s:
+                        next_step.add((max(seed_min,s)+offset,min(seed_max,s+r)+offset))
+                        print('added intersect', (max(seed_min,s)+offset,min(seed_max,s+r)+offset))
+                    # left:
+                    if seed_min<s and seed_min <min(s, seed_max): 
+                        curr_remain.add((seed_min, min(s, seed_max)))
+                        print('added left',(seed_min, min(s, seed_max)))
+                    # right 
+                    if seed_max >=s+r and max(s+r,seed_min)< seed_max:
+                        curr_remain.add((max(s+r,seed_min), seed_max))
+                        print('added right',(max(s+r,seed_min), seed_max))
+            curr_remain =curr_remain.union(next_step)
+            
+        return seed_ranges
+                    
+    min_loc = float('inf')
+    tmp = seed_range_to_location_min(set(seeds))
+    for seed_range in tmp:
+        min_loc= min(min_loc, seed_range[0])
+        
+    return min_loc
 
 
-    return sum(card_number)
-print(func2('test.txt'))
+assert func2() ==46, func2()
 print(func2('input.txt'))
+
--- day6/main.py
+++ day6/main.py
@@ -1,106 +1,67 @@
+"""
+Time:      7  15   30 (lasts     milli-sec
+Distance:  9  40  200 (recorded / milli-meter
+
+"""
+
 def func1(fname='test.txt'):
-    fourdarry = []
+    ans = 1
+    array = [[],[]]
     with open(fname) as f:
-        iterf = iter(f)
-        seeds = next(iterf) 
-        seeds = [int(n) for n in seeds.strip().split(' ')[1:]]
-        print(seeds)
-        nmap = -1
-        for line in f:
-            if line == '\n':
-                fourdarry.append([])
-                nmap += 1
-            else:
-                fourdarry[nmap].append(line.strip().split())
-                    
-                
-    def seed_to_location(seed):
-        print('seed is :',seed)
-        for si,step in enumerate(fourdarry):
-            for ci, choice in enumerate(fourdarry[si][1:]):
-                # print(f'choice:{choice}')
-                [d, s, r] = [int(n) for n in choice]
-                if s<= seed< s+r:
-                    seed+= (d-s)
-                    break
-        return seed
+        for  idx,line, in enumerate(f):
+            array[idx]=[int(n) for n in line.strip().split(':')[1].strip().split()]
+    [time, distance]=array 
+    def cal_range(t,d): # get range length
+        r =0 
+        begin,end = 0,0
+        for hold in range(1,t):
+            if -hold**2+t*hold > d:
+                if begin == 0:
+                    begin = hold
+                r+=1
+            end = hold
+        # print(begin,end)
+        return r
+    for i in range(len(time)):
+        # print(time[i],distance[i])
+        # print(cal_range(time[i],distance[i]))
+        ans*=cal_range(time[i],distance[i])
     
-    ans = 0
-    min_loc = float('inf')
-    for seed in seeds:
-        tmp = seed_to_location(seed)
-        if tmp < min_loc:
-            min_loc=tmp
-            ans = seed
-        
-        
-    return min_loc
-
-        
+    return ans
 
 
+# assert func1('test.txt') == 288
 # print(func1('input.txt'))
-        
+
+"""
+Time:      71530
+Distance:  940200
+"""
+
 def func2(fname='test.txt'):
-    fourdarry = []
+    ans = 1
+    array = [[],[]]
     with open(fname) as f:
-        iterf = iter(f)
-        seeds = next(iterf) 
-        seeds = [int(n) for n in seeds.strip().split(' ')[1:]]
-        seeds_pair = [seeds[npair:npair+2] for npair in range(0,len(seeds),2)]
-        seeds_pair =  [(x, x+y) for (x,y) in seeds_pair] # convert to range
-        seeds = seeds_pair
-        print(seeds)
-        nmap = -1
-        for line in f:
-            if line == '\n':
-                fourdarry.append([])
-                nmap += 1
-            else:
-                fourdarry[nmap].append(line.strip().split())
-                    
-                
-
+        for  idx,line, in enumerate(f):
+            array[idx]=[int(line.strip().split(':')[1].replace(' ','').strip())]
+    [time, distance]=array 
+    def cal_range(t,d): # get range length
+        r =0 
+        begin,end = 0,0
+        for hold in range(1,t):
+            if -hold**2+t*hold > d:
+                if begin == 0:
+                    begin = hold
+                r+=1
+            end = hold
+        # print(begin,end)
+        return r
+    for i in range(len(time)):
+        print('time and range',time[i],distance[i])
+        # print(cal_range(time[i],distance[i]))
+        ans*=cal_range(time[i],distance[i])
     
-    def seed_range_to_location_min(seed_ranges):
-        # seed_range is a [] contains range e.g. [(1,3),(5,8)]
-        curr_remain= seed_ranges.copy()
+    return ans
 
-        for si,step in enumerate(fourdarry):
-            # hit = 0
-            next_step = set()
-            for ci, choice in enumerate(step[1:]):
-                [d, s, r] = [int(n) for n in choice]
-                print(f'choice {ci}', choice)
-                offset = d-s
-                seed_ranges = curr_remain.copy()
-                for  seed_minmax in seed_ranges:
-                    (seed_min, seed_max)  = seed_minmax
-                    curr_remain.remove(seed_minmax)
-                    # intersection:
-                    if max(seed_min,s) < min(seed_max,s+r) and seed_max > s:
-                        next_step.add((max(seed_min,s)+offset,min(seed_max,s+r)+offset))
-                        print('added intersect', (max(seed_min,s)+offset,min(seed_max,s+r)+offset))
-                    # left:
-                    if seed_min<s and seed_min <min(s, seed_max): 
-                        curr_remain.add((seed_min, min(s, seed_max)))
-                        print('added left',(seed_min, min(s, seed_max)))
-                    # right 
-                    if seed_max >=s+r and max(s+r,seed_min)< seed_max:
-                        curr_remain.add((max(s+r,seed_min), seed_max))
-                        print('added right',(max(s+r,seed_min), seed_max))
-            curr_remain =curr_remain.union(next_step)
-            
-        return seed_ranges
-                    
-    min_loc = float('inf')
-    tmp = seed_range_to_location_min(set(seeds))
-    for seed_range in tmp:
-        min_loc= min(min_loc, seed_range[0])
-        
-    return min_loc
-
-
-assert func2() ==46, func2()
+assert func2('test.txt') == 71503
 print(func2('input.txt'))
-
